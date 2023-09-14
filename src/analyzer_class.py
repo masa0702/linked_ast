ec8507dc39a22bbc4bc3b62f33d1f51ba1e0506f
masa0702 <gp20a163@oecu.jp>
2023-09-14 16:48:21 +0900

    add Aanlyzer class

--- src/analyzer_class.py
+++ src/analyzer_class.py
@@ -1,2 +1,23 @@
+import pydot
+import os
+import json
+from tree_sitter import Language, Parser
+from anytree import Node, RenderTree
 
-                    
+class Analyzer:
+    def extract_function_calls(self, node, filename):
+        if node["type"] == "call":
+            for child in node["children"]:
+                if child["type"] == "attribute":
+                    function_name = None
+                    alias_name = None
+                    for grandchild in child["children"]:
+                        if grandchild["type"] == "identifier":
+                            if alias_name is None:
+                                alias_name = grandchild["content"]
+                            else:
+                                function_name = grandchild["content"]
+        for child in node.get("children", []):
+            self.extract_function_calls(child, filename)
+            
+    
