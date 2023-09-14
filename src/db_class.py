a769b111b888e421233c5875f436b29c1ef32961
masa0702 <gp20a163@oecu.jp>
2023-09-14 17:23:33 +0900

    add db class

--- src/db_class.py
+++ src/db_class.py
@@ -2,7 +2,30 @@ import sqlite3
 import os
 
 class Db:
-    def show_all_db_contents(self, db_dir):
+    def remove_duplicate_rows_keep_one(self, db_path):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        
+        cursor.execute("CREATE TABLE my_table_temp AS SELECT DISTINCT * FROM my_table")
+        cursor.execute("DROP TABLE my_table")
+        cursor.execute("ALTER TABLE my_table_temp RENAME TO my_table")
+        conn.commit()
+        conn.close()
+    
+    def create_db_import_elements(self, db_path, table_elements):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        # テーブル作成のSQL文を動的に生成
+        create_table_sql = f"CREATE TABLE IF NOT EXISTS my_table ({', '.join(table_elements)})"
+        
+        cursor.execute(create_table_sql)
+        conn.commit()
+        conn.close()
+        
+    
+    
+    def show_all_dbs_contents(self, db_dir):
         for filename in os.listdir(db_dir):
             if filename.endwith(".db"):
                 db_path = os.path.join(db_dir, filename)
@@ -18,3 +41,25 @@ class Db:
                     print(row)
                 conn.close()
                 print()
+                
+    def show_db_contents(self, db_path):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table':")
+        tables = cursor.fetchall()
+        for table in tables:
+            table_name = table[0]
+            print(f"Table : {table_name}")
+            print("--------------------")
+            
+            cursor.execute(f"SELECT *FROM {table_name}")
+            rows = cursor.fetchall()
+            column_names = [description[0] for description in cursor.description]
+            print("\t".join(column_names))
+            
+            for row in rows:
+                print("\t".join(str(col) for col in row))
+            print()
+            conn.close()
+            
+    
